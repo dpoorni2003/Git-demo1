7e5e6915186f7a84acbe91bda24e7b958c3f20ab
dpoor <dpoor@192.168.2.13>
2023-01-05 23:10:01 -0500

    first commit;

---
+++ src/main/java/demoPackage/Sample.java
@@ -0,0 +1,11 @@
+package demoPackage;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("This is my First Github");
+		// TODO Auto-generated method stub
+
+	}
+
+}
